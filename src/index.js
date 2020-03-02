19538bf3315a6a67e6126360ae7198f2ef6a0d56
Vladimir Ermolin <vladimir.ermolin@fastdev.se>
2020-03-03 00:07:00 +0400

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let config = bracketsConfig.map(config => config.join(''));
+    let length = null;
+    do {
+        length = str.length;
+        for (let i = 0; i < config.length; i++) {
+            str = str.replace(config[i], '');
+        }
+    } while (length !== str.length);
+    return !str.length;
+};
